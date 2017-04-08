2301df03ad0813b9427b042921af016a8be9c950
rahulbhatia023 <rahul.bhatia023@gmail.com>
2017-04-08 16:56:49 +0530

    Lambda Expression

--- src/main/java/com/rahul/javabrains/Greeter.java
+++ src/main/java/com/rahul/javabrains/Greeter.java
@@ -3,9 +3,16 @@ package com.rahul.javabrains;
 public class Greeter {
     public static void main(String args[]) {
         Greeting helloWorldGreeting = new HelloWorldGreeting();
+        Greeting innerClassGreeting = new Greeting() {
+            @Override
+            public void greet() {
+                System.out.println("Hello World");
+            }
+        };
         Greeting lambdaGreeting = () -> System.out.println("Hello World");
 
         helloWorldGreeting.greet();
+        innerClassGreeting.greet();
         lambdaGreeting.greet();
     }
 }
